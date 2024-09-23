f9b4deca642f3f8ada61f7a4c2ff0f2fcd0269fa
S-K-Hasan <syedkhairulhaan123@gmail.com>
2024-09-23 16:16:17 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-login').addEventListener('click',function(event){
+   event.preventDefault(); // vejal
+   
+   const phoneNumber=document.getElementById('phone-number').value;
+//    console.log(phoneNumber);
+    const pinNumber=document.getElementById('pin-number').value;
+    // demo
+    if(phoneNumber==='01703035415' && pinNumber==='98765')
+    {
+     alert('congratulation!!!');
+     window.location.href='/homepage.html'
+    }
+    else{
+        alert('Wrong phone nymber or pin.Please enter correct phone and pin!!!');
+    }
+})
